d36cef3f9ae38bfdfcacab8c3a42783a191ccbc7
Bai Sakina Abad <kimireneabad07@gmail.com>
2026-02-24 01:06:12 +0000

    change 1200 into 200

--- BPI_Atm.java
+++ BPI_Atm.java
@@ -2,7 +2,7 @@ public class BPI_Atm {
     public static void main(String[] args) {
         ATMDispenseChain atmDispenser = new ATMDispenseChain();
       
-        int amount = 70; // Amount to be dispensed
+        int amount = 290; // Amount to be dispensed
           System.out.println("Amount: " + amount);
         if (amount % 10 != 0) {
             System.out.println("Amount should be in multiples of 10s.");
